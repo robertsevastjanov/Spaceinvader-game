03ad30ed0623ec7b6c4b2e277b32dcf224ab497c
Roberts <robert@MacBook-Air-Roberts.local>
2026-07-17 04:52:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+# Initialize the pygame
+pygame.init()
+
+#create the screen
+screen = pygame.display.set_mode((800, 600))
+
+#Title and Icon
+pygame.display.set_caption("Space Invader")
+icon = pygame.image.load("launch.png")
+pygame.display.set_icon(icon)
+
+#Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # RGB = Red, Green, Blue
+    screen.fill((15, 30, 200))
+    pygame.display.update()
