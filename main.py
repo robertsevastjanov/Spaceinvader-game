d7606b1091ff1c7b2e22d1e67a217cdd004d08db
Roberts <robert@MacBook-Air-Roberts.local>
2026-07-17 07:20:58 +0300

    bullet appeared

--- main.py
+++ main.py
@@ -28,11 +28,24 @@ enemyY = random.randint(0,150)
 enemyX_change = 4
 enemyY_change = 40
 
+# Bullet
+bulletImg = pygame.image.load("bullet.png")
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 4
+bullet_state = "ready"
+
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 def enemy(x, y):
     screen.blit(enemyImg, (x, y))
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x, y))
 
 #Game Loop
 running = True
@@ -51,6 +64,12 @@ while running:
                 playerX_change = -5
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
+            if event.key == pygame.K_SPACE:
+                if bullet_state is "ready":
+
+                    bulletX = playerX
+                    fire_bullet(playerX, bulletY)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -68,6 +87,15 @@ while running:
     elif enemyX >= 770:
         enemyX_change = -4
         enemyY += enemyY_change
+
+    #Bullet movement
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+
+    if bullet_state is "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
