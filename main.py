0af36566c9a9aed849d839a3754b78eec09f6a75
Roberts Sevastjanovs <47148502+robertsevastjanov@users.noreply.github.com>
2026-07-17 04:33:07 +0300

    Add title and icon

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ pygame.init()
 #create the screen
 screen = pygame.display.set_mode((800, 600))
 
+#Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("launch.png")
+pygame.display.set_icon(icon)
+
 #Game Loop
 running = True
 while running:
