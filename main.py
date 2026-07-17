01e25f2e56e2227ee5347659e2f27caf3d68f80e
Roberts <robert@MacBook-Air-Roberts.local>
2026-07-17 05:10:54 +0300

    Add player

--- main.py
+++ main.py
@@ -11,13 +11,24 @@ pygame.display.set_caption("Space Invader")
 icon = pygame.image.load("launch.png")
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load("spaceship.png")
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
 #Game Loop
 running = True
 while running:
+    # RGB = Red, Green, Blue
+    screen.fill((15, 30, 200))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # RGB = Red, Green, Blue
-    screen.fill((15, 30, 200))
+
+    player()
     pygame.display.update()
